bbc1484ed32b4537f5279ef74a0460a0321f530e
saimohansattari <chinnasattari@gmail.com>
2023-07-24 21:43:20 +0530

    build fixes

--- src/assets/Our-patners-images/index.js
+++ src/assets/Our-patners-images/index.js
@@ -4,5 +4,5 @@ export { default as Chola } from "./chola.png";
 export { default as Decentro } from "./Decentro.png";
 export { default as Digio } from "./Digio.png";
 export { default as Finalyca } from "./Finalyca.png";
-export { default as Kfintech } from "./Kfintech.png";
+export { default as Kfintech } from "./KFinTech.png";
 export { default as Searce } from "./Searce.png";
