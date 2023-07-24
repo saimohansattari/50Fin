4269ddde3b39bfc05b1df254f962e70b54ef39fd
saimohansattari <chinnasattari@gmail.com>
2023-07-24 21:37:12 +0530

    changes done

--- src/assets/Our-patners-images/index.js
+++ src/assets/Our-patners-images/index.js
@@ -1,6 +1,6 @@
 export { default as Bajaj } from "./Bajaj.png";
 export { default as Cams } from "./Cams.png";
-export { default as Chola } from "./Chola.png";
+export { default as Chola } from "./chola.png";
 export { default as Decentro } from "./Decentro.png";
 export { default as Digio } from "./Digio.png";
 export { default as Finalyca } from "./Finalyca.png";
